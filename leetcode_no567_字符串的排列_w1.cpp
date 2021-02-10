ebda55bc4be860541468ab5425cb98f1b4413507
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-10 09:51:20 +0800

    Add files via upload

---
+++ leetcode_no567_字符串的排列_w1.cpp
@@ -0,0 +1,100 @@
+﻿/* ------------------------------------------------------------------|
+给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的排列。
+
+换句话说，第一个字符串的排列之一是第二个字符串的子串。
+
+示例1:
+
+输入: s1 = "ab" s2 = "eidbaooo"
+输出: True
+解释: s2 包含 s1 的排列之一 ("ba").
+ 
+
+示例2:
+
+输入: s1= "ab" s2 = "eidboaoo"
+输出: False
+ 
+
+注意：
+
+输入的字符串只包含小写字母
+两个字符串的长度都在 [1, 10,000] 之间
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/permutation-in-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：8.1 MB, 在所有 C++ 提交中击败了81.94%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+bool checkInclusion(string s1, string s2) {
+    if (s2.size() < s1.size()) {
+        return false;
+    }
+    vector<int> v1(26, 0), v2(26, 0);
+    for (int i = 0; i < s1.size(); i++) {
+        v1[s1[i] - 'a']++;
+        v2[s2[i] - 'a']++;
+    }
+
+    for (int i = 0, j = s1.size(); j < s2.size(); i++, j++) {
+        if (v1 == v2) {
+            return true;
+        }
+        v2[s2[i] - 'a']--;
+        v2[s2[j] - 'a']++;
+        
+    }
+    return v1 == v2;
+}
+
+int main() {
+    string s1 = "abc";
+    string s2 = "ccccbbbbaaaa";
+    checkInclusion(s1, s2);
+}
